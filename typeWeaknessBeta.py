d9e22ab64e6d8049959515b28207fcec713d0584
C. Xie <32278569+cxie00@users.noreply.github.com>
2018-08-08 14:29:06 -0400

    other types added

--- typeWeaknessBeta.py
+++ typeWeaknessBeta.py
@@ -3,16 +3,25 @@
 
 
 weakness = {
-    "fire" : [],
-    "water" : [],
-    "grass" : [],
-    "ice" : [],
-    "bug" : [],
-    "ghost" : [],
-    "dark" : [],
-    "psychic" : [],
-    "fighting" : [],
-    "steel" : [],
-    "fairy" : [],
-    "
+    "fire" : ["ground", "water", "rock"],
+    "water" : ["electric", "grass"],
+    "grass" : ["bug", "fire", "flying", "ice", "poison"],
+    "ice" : ["fight", "fire", "rock", "steel"],
+    "bug" : ["fire", "flying", "rock"],
+    "ghost" : ["dark", "ghost"],
+    "dark" : ["bug", "fairy", "fighting"],
+    "psychic" : ["dark", "ghost", "bug"],
+    "fighting" : ["flying", "fairy", "psychic"],
+    "steel" : ["fight", "fire", "ground"],
+    "fairy" : ["poison", "steel"],
+    "dragon" : ["ice", "dragon", "fairy"],
+    "poison" : ["psychic", "ground"],
+    "ground" : ["water", "grass", "ice"],
+    "rock" : ["fighting", "grass", "ground", "steel", "water"],
     }
+
+def sWeakness(type_a):
+    if type_a in weakness:
+        print(weakness[type_a])
+    else:
+        print("Sorry, " + type_a + " is unrecognized.")
